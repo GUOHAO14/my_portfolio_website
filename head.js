0842f277d0dd049c553c048fb3e994b2fe1eaf64
GUOHAO14 <guohao.k@hotmail.com>
2026-05-05 23:36:10 +0800

    favicon & title

---
+++ head.js
@@ -0,0 +1,8 @@
+// head.js
+document.title = "Guo Hao Khoo | Portfolio";
+
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/svg+xml";
+favicon.href = "images/websiteLogo.png";
+document.head.appendChild(favicon);
